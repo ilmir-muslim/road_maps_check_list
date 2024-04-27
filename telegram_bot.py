523e5351b1fb7a48208d83c9f42a2183fcd7c31e
ilmir <ilmirirakti@mail.com>
2024-04-27 12:50:37 +0300

    initial commit

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,4 +8,9 @@ def start(message):
     bot.send_message(message.chat.id, 'Категорически приветствую, бот запущен!')
 
 
+def on_new_chat_members(update):
+    new_members = update.message.new_chat_members
+    for member in new_members:
+        update.message.reply_text(f"Добро пожаловать, {member.first_name}!")
+
 bot.polling(none_stop=True)
