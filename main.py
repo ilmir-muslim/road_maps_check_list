c5d7c751cfa3382fb603e287178b688ad67de414
ilmir <ilmirirakti@mail.com>
2024-05-19 16:32:15 +0300

    коммит для сохранения текущего прогресса, как собирают полностью изменить подход

--- main.py
+++ main.py
@@ -1,34 +1,98 @@
-from datetime import datetime
-import telebot
-from studentdata import StudentData
+import asyncio
+import logging
 import schedule
+import threading
+import telebot
+import time
+import json
 import os
+from datetime import datetime
 
-TOKEN = os.getenv('BOT_TOKEN')
-bot = telebot.TeleBot(TOKEN)
+from aiogram import Bot, Dispatcher, types
+from aiogram import F
+from aiogram.filters.command import Command
+from aiogram.dispatcher.router import Router
+
+from studentdata import StudentData
+
+logging.basicConfig(level=logging.INFO)
+API_TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+router = Router()
 
 
-@bot.message_handler(content_types=['new_chat_members'])
-async def on_new_chat_members(update):
-    # добавление нового участника
+@dp.message(F.new_chat_members)
+async def on_new_chat_members(message):
+    # добавление новых участников
+    for user in message.new_chat_members:
+        await bot.send_message(message.chat.id, f"У нас новенький - {user.full_name}, привет 🙂!")
+        join_date = datetime.utcfromtimestamp(message.date.timestamp()).strftime('%d.%m.%Y')
+        users_name = user.full_name
+        await StudentData().add_data_to_json(users_name, join_date)
 
-    await update.message.reply_text(f"У нас новенький - , {update.first_name}, привет 🙂!")
-    join_date = datetime.utcfromtimestamp(update.date).strftime('%d-%m-%Y')
-    users_name = update.first_name
-    await StudentData().add_data_to_json(users_name, join_date)
 
-
-@bot.message_handler(commands=['start'])
-def on_start(message):
+@dp.message(Command('start'))
+async def on_start(message: types.Message):
     # приветствие пользователя
 
-    bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}!")
+    await message.answer(f"бот запущен ID группы, {message.chat.id}")
+    chat_id = message.chat_id
 
-
-@bot.message_handler(commands=['update'])
-def on_update(message):
+@dp.message(Command('update'))
+async def on_update(message):
     StudentData().update_json()
-    bot.send_message(message.chat.id, f"Данные обновлены!")
+    await bot.send_message(message.chat.id, f"Данные обновлены!")
 
 
-bot.infinity_polling(none_stop=True)
+with open('data_tests/road_map.json', 'r') as file:
+    road_map = json.load(file)
+
+with open('data_tests/members.json', 'r') as file:
+    members = json.load(file)
+
+
+async def send_messages(member, message):
+    await bot.send_message(member['name'], message)
+
+
+@router.message(Command('progress'))
+async def personal_message_for_all_members():
+    StudentData().update_json()
+
+    for member in members:
+
+        prev_progress = str(int(member['current_progress']) - 1)
+        if prev_progress in road_map:
+            await send_messages(member, 'v предыдущий этап: ' + road_map[prev_progress])
+        else:
+            print(f"Ключ {prev_progress} не существует в словаре road_map.")
+
+        await send_messages(member, '> текущий этап: ' + road_map.get(member['current_progress'], "Progress not found"))
+
+        next_progress = str(int(member['current_progress']) + 1)
+        if next_progress in road_map:
+            await send_messages(member, 'x следующий этап: ' + road_map[next_progress])
+        else:
+            print(f"Ключ {next_progress} не существует в словаре road_map.")
+
+
+# schedule.every().day.at("09:48").do(personal_message_for_all_members)
+
+
+def run_schedule():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+thread = threading.Thread(target=run_schedule)
+thread.start()
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- studentdata.py
+++ studentdata.py
@@ -11,9 +11,11 @@ class StudentData:
         with open(self.members_json, "r") as file:
             members = json.load(file)
 
-        for i in range(len(members)):
-            members[i]['current_progress'] = self.studying_days(members[i]['added_date'])
-            members[i]['studying_stage'] = self.studying_stage(members[i]['current_progress'])
+        for i, member in enumerate(members):
+            if int(self.studying_days(member['added_date'])) < 91:
+                members[i]['current_progress'] = self.studying_days(members[i]['added_date'])
+            elif members[i]['current_progress'] > 90:
+                members.pop(i)
 
         with open(self.members_json, "w") as file:
             json.dump(members, file, ensure_ascii=False, indent=4)
@@ -25,13 +27,13 @@ class StudentData:
         stage = road_map.get(str(studying_days))
         return stage
 
-    def add_data_to_json(self, name: str, added_date: str):
+    async def add_data_to_json(self, name: str, added_date: str):
         with open(self.members_json, "r") as file:
             participants = json.load(file)
         participants.append({"name": name, "added_date": added_date, "current_progress": "1",
                              "studying_stage": 'установка python и IDE, первая программа "Hello World!"'})
         with open(self.members_json, "w") as file:
-            json.dump(participants, file)
+            json.dump(participants, file, ensure_ascii=False, indent=4)
 
     @staticmethod
     def studying_days(added_date: str):
@@ -41,3 +43,10 @@ class StudentData:
             date = datetime.datetime.strptime('01.01.2000', '%d.%m.%Y')
         today = datetime.datetime.now()
         return (today - date).days
+
+    def get_data_from_json(self, name: str):
+        with open(self.members_json, "r") as file:
+            members = json.load(file)
+            if name in members:
+                stage = members['name']['studying_stage']
+                return stage
